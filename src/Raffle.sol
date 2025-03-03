f764471a0a2d1858e7cb42ce82dd14327d9093c7
seyedmahdihosseini06@gmail.com <seyedmahdihosseini06@gmail.com>
2025-03-03 16:10:13 +0330

    feat: implement enterRaffle logic with validation, events, and player storage

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
@@ -8,13 +29,27 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
