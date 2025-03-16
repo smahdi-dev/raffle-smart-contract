9cb09c7e342dd032ca883c5e0e88c71de9d229e5
seyedmahdihosseini06@gmail.com <seyedmahdihosseini06@gmail.com>
2025-03-16 16:25:53 +0330

    reset s_player s_lastTimeStamp emitted WinnerPicked event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -58,6 +58,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
 
     /* Events */
     event RaffleEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -114,6 +115,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
         s_recentWinner = recentWinner;
 
         s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;  
+        emit WinnerPicked(s_recentWinner);
 
         (bool success,) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
